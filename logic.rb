38c2075ec2dab22643b58c352c89b395f40bfeaf
Jimno <menaoir.jimmy@gmail.com>
2022-10-17 18:01:44 +0100

    fixed case-sensitivty, symbols and some minor issues

--- logic.rb
+++ logic.rb
@@ -1,34 +1,38 @@
 def caesar_cipher(string, shift_key)
     letters_az = ('a'..'z').to_a
-    characters = [" ", "!"]
-    letters = string.downcase.split("")
+    symbols = ["!", " ", "?", ":", "\'"]
+    letters = string.split("")
     array = []
 
     letters.map do |letter|
         letters_az.each_with_index do |alphabet, index|
 
-            if  letter == alphabet
+            if  letter.downcase == alphabet
                 new_index = index + shift_key 
-                if  new_index >= 26 
+                if  new_index >= 26
                     new_index = new_index - 26 
                 end
+                if shift_key > 25
+                    puts "Shift-Key limit exceeded! (25)"
+                    return
+                end
                 result = letters_az[new_index]
-                array << result   
+                if  letter == alphabet
+                    array << result 
+                else
+                    array << result.upcase
+                end
             end  
         end
-
-        characters.each do |character|
-            if letter == " "
-                result = characters[0]
+        symbols.each_with_index do |character, index|
+            if letter == character
+                result = symbols[index]
             end
-            if letter == "!"
-                result = characters[1]
-            end
-                $ring = "#{result}"
+            array << result
         end
-        array << $ring
     end
-    p array.join().capitalize
+    p array.join()
 end
-puts "Write the secret then the shift-key:"
+puts "Write the secret \"message\" then the shift-key:"
+
 caesar_cipher(gets.chomp, gets.to_i)
