2e2b0b261cf5d9761caccb46bbef8d70f4b80364
Jimno <menaoir.jimmy@gmail.com>
2022-10-30 16:25:29 +0100

    better style...

--- logic.rb
+++ logic.rb
@@ -6,27 +6,32 @@ def caesar_cipher(string, shift_key)
 
   letters.map do |letter|
     letters_az.each_with_index do |alphabet, index|
-      next unless letter.downcase == alphabet
 
-      new_index = index + shift_key
-      new_index -= 26 if new_index >= 26
-      if shift_key > 25
-        puts 'Shift-Key limit exceeded! (25)'
-        return
-      end
-      result = letters_az[new_index]
-      array << if letter == alphabet
-                 result
-               else
-                 result.upcase
-               end
+      if  letter.downcase == alphabet
+        new_index = index + shift_key 
+        if new_index >= 26
+          new_index = new_index - 26 
+        end
+        if shift_key > 25
+          puts 'Shift-Key limit exceeded! (25)'
+          return
+        end
+        result = letters_az[new_index]
+        if  letter == alphabet
+          array << result 
+        else
+          array << result.upcase
+        end
+      end  
     end
     symbols.each_with_index do |character, index|
-      result = symbols[index] if letter == character
-      array << result
+      if letter == character
+        result = symbols[index]
+      end
+        array << result
     end
   end
-  p array.join
+  p array.join()
 end
 puts 'Write the secret \"message\" then the shift-key:'
 
