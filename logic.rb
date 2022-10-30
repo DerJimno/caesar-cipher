91efb4da792cd6e790af52e9d28926924131321f
Jimno <menaoir.jimmy@gmail.com>
2022-10-30 15:36:17 +0100

    add better Style to ruby code

--- logic.rb
+++ logic.rb
@@ -1,38 +1,38 @@
 def caesar_cipher(string, shift_key)
-    letters_az = ('a'..'z').to_a
-    symbols = ["!", " ", "?", ":", "\'"]
-    letters = string.split("")
-    array = []
+  letters_az = ('a'..'z').to_a
+  symbols = ['!', ' ', '?', ':', '\'']
+  letters = string.split('')
+  array = []
 
-    letters.map do |letter|
-        letters_az.each_with_index do |alphabet, index|
+  letters.map do |letter|
+    letters_az.each_with_index do |alphabet, index|
 
-            if  letter.downcase == alphabet
-                new_index = index + shift_key 
-                if  new_index >= 26
-                    new_index = new_index - 26 
-                end
-                if shift_key > 25
-                    puts "Shift-Key limit exceeded! (25)"
-                    return
-                end
-                result = letters_az[new_index]
-                if  letter == alphabet
-                    array << result 
-                else
-                    array << result.upcase
-                end
-            end  
+      if  letter.downcase == alphabet
+        new_index = index + shift_key 
+        if new_index >= 26
+          new_index = new_index - 26 
         end
-        symbols.each_with_index do |character, index|
-            if letter == character
-                result = symbols[index]
-            end
-            array << result
+        if shift_key > 25
+          puts 'Shift-Key limit exceeded! (25)'
+          return
         end
+        result = letters_az[new_index]
+        if  letter == alphabet
+          array << result 
+        else
+          array << result.upcase
+        end
+      end  
     end
-    p array.join()
+    symbols.each_with_index do |character, index|
+      if letter == character
+        result = symbols[index]
+      end
+        array << result
+    end
+  end
+  p array.join()
 end
-puts "Write the secret \"message\" then the shift-key:"
+puts 'Write the secret \"message\" then the shift-key:'
 
 caesar_cipher(gets.chomp, gets.to_i)
